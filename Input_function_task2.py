1e0f31992bdcb41118afda2b8590415824f703ca
parag.veerkar <same-email-as-your-github-account@example.compsvirkar@gmail.com>
2026-07-23 08:22:45 +0530

    F string task

---
+++ Input_function_task2.py
@@ -0,0 +1,13 @@
+stock = input ("Which Stock you are scanning ? ")
+print("it is", stock)
+Stock_Price = float(input(f"What is current price of {stock}?"))
+print (f"current price of {stock} is", Stock_Price)
+Qty = 1000
+price_upper_range = 1300
+price_lower_range = 1250
+if Stock_Price >= price_upper_range:
+    print ("Price of", stock, "is above", price_upper_range, "Buy", Qty)
+elif Stock_Price <= price_lower_range:
+    print ("Price of", stock,"is below", price_lower_range, "Sell", Qty)
+else:
+    print ("price of", stock, "is range bound between",price_lower_range,"and",price_upper_range, "hence Hold the current position")
